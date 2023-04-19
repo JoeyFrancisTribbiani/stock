8b3b2cf90b476e2d7bda507686d814b239178b8c
joey <joey@fuck.com>
2023-04-19 16:26:42 +0800

    yy

--- src/main/java/com/yy/stock/bot/aliexpressbot/brazilbot/AliExpressBrrCoreEngine.java
+++ src/main/java/com/yy/stock/bot/aliexpressbot/brazilbot/AliExpressBrrCoreEngine.java
@@ -13,8 +13,8 @@ import com.yy.stock.common.util.MySpringUtil;
 
 import java.math.BigDecimal;
 
-public class AliExpressBrCoreEngine extends AliExpressCoreEngine {
-    public AliExpressBrCoreEngine() {
+public class AliExpressBrrCoreEngine extends AliExpressCoreEngine {
+    public AliExpressBrrCoreEngine() {
         this.xpaths = MySpringUtil.getBean(AliExpressXpaths.class);
         this.urls = MySpringUtil.getBean(AliExpressUrls.class);
         this.classSelector = MySpringUtil.getBean(AliExpressClassSelectors.class);
